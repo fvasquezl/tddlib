fbcd22944d79bcb60a4969d831063d15cd0b609a
Faustino Vasquez Limon <vasquez.limon@gmail.com>
2020-11-02 13:13:37 -0800

    Post Fields Required

--- tests/Feature/Posts/CreatePostsTest.php
+++ tests/Feature/Posts/CreatePostsTest.php
@@ -63,4 +63,67 @@ class CreatePostsTest extends TestCase
         $this->assertDatabaseMissing('posts', $post);
     }
 
+    /**  @test */
+    public function categories_must_be_a_relationship_object()
+    {
+        $post = Post::factory()->raw(['category_id' => null]);
+        $post['categories'] = 'slug';
+
+        Sanctum::actingAs(User::factory()->create());
+
+        $this->jsonApi()->withData([
+            'type' => 'posts',
+            'attributes' => $post
+        ])->post(route('api.v1.posts.create'))
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/categories');
+        ;
+        $this->assertDatabaseMissing('posts', $post);
+    }
+
+    /**  @test */
+    public function title_is_required()
+    {
+        $post = Post::factory()->raw(['title' => '']);
+        Sanctum::actingAs(User::factory()->create());
+        $this->jsonApi()->withData([
+            'type' => 'posts',
+            'attributes' => $post
+        ])->post(route('api.v1.posts.create'))
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/title');
+
+        $this->assertDatabaseMissing('posts', $post);
+    }
+
+    /**  @test */
+    public function excerpt_is_required()
+    {
+        $post = Post::factory()->raw(['excerpt' => '']);
+        Sanctum::actingAs(User::factory()->create());
+        $this->jsonApi()->withData([
+            'type' => 'posts',
+            'attributes' => $post
+        ])->post(route('api.v1.posts.create'))
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/excerpt');
+
+        $this->assertDatabaseMissing('posts', $post);
+    }
+
+    /**  @test */
+    public function slug_is_required()
+    {
+        $post = Post::factory()->raw(['slug' => '']);
+        Sanctum::actingAs(User::factory()->create());
+        $this->jsonApi()->withData([
+            'type' => 'posts',
+            'attributes' => $post
+        ])->post(route('api.v1.posts.create'))
+            ->assertStatus(422)
+            ->assertSee('data\/attributes\/slug');
+
+        $this->assertDatabaseMissing('posts', $post);
+    }
+
 }
